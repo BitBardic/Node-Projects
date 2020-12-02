b0c15760994533e9d6417bbc66b2a8d0d057652b
Nasir Iqbal <nasirpucit@hotmail.com>
2020-12-02 00:31:55 +0000

    added a very simple API

--- complete-node-bootcamp-master/1-node-farm/index.js
+++ complete-node-bootcamp-master/1-node-farm/index.js
@@ -36,6 +36,10 @@ const url = require('url');
 
 ///////////////////////////////////////////
 // SERVER
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+    
 const server = http.createServer((req, res) => {
     console.log(req.url);
 
@@ -45,6 +49,9 @@ const server = http.createServer((req, res) => {
         res.end('This is the OVERVIEW');
     } else if (pathName === '/product') {
         res.end('This is the PRODUCT');
+    } else if (pathName === '/api') {
+        res.writeHead(200, { 'Content-type': 'application/json'});
+        res.end(data);       
     } else {
         res.writeHead(404, {
             'Content-type': 'text/html',
@@ -53,7 +60,7 @@ const server = http.createServer((req, res) => {
         res.end('<h1>Page not found!</h1>');
     }
     //console.log(req);
-    res.end('Hello from the server!');
+    //res.end('Hello from the server!');
 });
 
 server.listen(8000, '127.0.0.1', () => {
