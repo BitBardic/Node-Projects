474bedf9622eb184d11373240adc21abe0858ae8
Nasir Iqbal <nasirpucit@hotmail.com>
2020-12-16 17:50:23 +0000

    Making the API better pagination

--- complete-node-bootcamp-master/4-natours/controller/tourController.js
+++ complete-node-bootcamp-master/4-natours/controller/tourController.js
@@ -41,6 +41,18 @@ exports.getAllTours = async (req, res) => {
       query = query.select('-__v');
     }
 
+    // 4) Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+    //page=2&limit=50, 1-10 => page 1, 11-20 => page 2, page 3 => 21-30
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exists');
+    }
+
     // EXECUTE QUERY
     const tours = await query;
 
