f157817b8a3a923994abf590463965f0a92b4d3b
Nasir Iqbal <nasirpucit@hotmail.com>
2020-12-19 17:33:58 +0000

    Data validation built-in validators

--- complete-node-bootcamp-master/4-natours/controller/tourController.js
+++ complete-node-bootcamp-master/4-natours/controller/tourController.js
@@ -74,7 +74,7 @@ exports.createTour = async (req, res) => {
   } catch (err) {
     res.status(400).json({
       status: 'fail',
-      message: 'Invalid data sent!',
+      message: err
     });
   }
 };
@@ -83,6 +83,7 @@ exports.updateTour = async (req, res) => {
   try {
     const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
+      runValidators: true
     });
     res.status(200).json({
       status: 'success',
