9f339da4c0d9e47b4341a250512f0e9acadae9d4
Nasir Iqbal <nasirpucit@hotmail.com>
2020-12-30 17:19:03 +0000

    Creating new users

--- complete-node-bootcamp-master/4-natours/routes/userRoutes.js
+++ complete-node-bootcamp-master/4-natours/routes/userRoutes.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const userController = require('./../controller/userController');
+const authController = require('./../controller/authController');
 const router = express.Router();
+
+router.post('/signup', authController.signup);
 
 router
 .route('/')
